105fe58f80fff1f1e632fd3e2b315b47c744a623
Knutschbert <knutschbert@gmail.com>
2025-05-01 17:05:51 +0200

    use global timeout setting. Replace forms with menus+events for Entry Point and Paintings stages

---
+++ start_dialogmanager.bat
@@ -0,0 +1,17 @@
+@echo off
+
+set OC_LICENSE=OTVB.uni_Magdeburg_IESK.lic
+
+REM :: -e erase logfiles at startup
+REM :: -s0 severity level of logged messages (0-6)
+REM :: path to ccxml-file
+REM ot_ccxml_interpreter -e Files/simple.ccxml
+
+@REM chdir Optimtalk
+@REM ot_vxml_interpreter.exe -e Files/menu.vxml
+@REM ot_vxml_interpreter.exe -e ../../Weihnachtsmarkt.vxml 
+pushd ..\Dialogmanager\Optimtalk
+ot_vxml_interpreter.exe -s5 -e -s0 ../../test_vxml/Files/puzzle.vxml
+popd
+PAUSE 
+
